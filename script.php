a53dd716b68146454cf00cc333569a8c2cf19bda
LeoneCarmelo <carmeloleone@outlook.it>
2023-05-03 16:22:47 +0200

    Show strings + length

--- script.php
+++ script.php
@@ -7,12 +7,14 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         echo "The paragraph is empty";
     } else {
         echo $paragraph;
-        echo ' ';
-        echo strlen($paragraph);
+        echo " The length of this word is ", strlen($paragraph);
         echo '<br>';
     }
     if (empty($badwords)) {
         echo "The badwords is empty";
+    } else if($badwords == 'fuck'){
+        echo str_replace('fuck', 'f***', $badwords );
+        echo " The badword's length is ", strlen($badwords);
     } else {
         echo $badwords;
         echo '<br>';
